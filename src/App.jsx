8471c1977e55e6078ddf3a9e5343534fcf71e539
Izadora <izadorasantosdesousa@gmail.com>
2023-04-29 20:46:35 -0300

    Projeto de filmes consumindo uma API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Movies from './components/Movies'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Movies/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import { BsFillCameraReelsFill } from "react-icons/bs";
+import './Header.css'
+const Header = () => {
+  return <header>
+    <h1>Filmes <BsFillCameraReelsFill/></h1>
+  </header>
+}
+
+export default Header
